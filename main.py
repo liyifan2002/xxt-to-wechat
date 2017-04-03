0a3fdc23925ca968ebe8c26224290cee76c17828
Ivan Lee <liyifan2002@users.noreply.github.com>
2017-04-03 14:25:23 +0800

    send to wechat

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import config
 import hashlib,base64,json,re
 import urllib.request,http.cookiejar
+import wechat_sdk
+
 def getOpener():
     cj = http.cookiejar.CookieJar()
     pro = urllib.request.HTTPCookieProcessor(cj)
     opener = urllib.request.build_opener(pro)
     return opener
+
 def b64en (data):
     return base64.b64encode(data.encode("utf-8")).decode()
 
@@ -15,17 +18,22 @@ def sha1withSalt (data,salt):
     return result
 
 username=b64en(config.xxt_username)
-logpre_url="http://login.xxt.cn/login/ajax/loginpre.do?&username="+username
-login_url="http://login.xxt.cn/login/ajax/login.do?entry=xxt_web&clientVersion=V0.5.0&cryptType=A&callbackFn=XXTSSO.showResult"
-get_url="http://jxlx.xxt.cn/jxlx/teacher/msgReceive.action?receiveNum=1"
+logpre_url = "http://login.xxt.cn/login/ajax/loginpre.do?&username="+username
+login_url = "http://login.xxt.cn/login/ajax/login.do?entry=xxt_web&clientVersion=V0.5.0&cryptType=A&callbackFn=XXTSSO.showResult"
+get_url = "http://jxlx.xxt.cn/jxlx/teacher/msgReceive.action?receiveNum=0"
+wx = wechat_sdk.WeChatEnterprise()
 opener = getOpener()
-logpre_req=opener.open(logpre_url)
-logpre_res=logpre_req.read().decode()
+logpre_req = opener.open(logpre_url)
+logpre_res = logpre_req.read().decode()
 jsonData = json.loads(logpre_res)
-key=jsonData["key"]
-password=sha1withSalt(config.xxt_password,key)
-login_url+="&username="+username+"&password="+password+"&key="+key
-login_req=opener.open(login_url)
-xxt_res = opener.open(get_url)
-msg = xxt_res.read().decode("gbk")
-print(re.findall("<span class=\"msgContent\">(.*)</span>",msg))
+key = jsonData["key"]
+password = sha1withSalt(config.xxt_password,key)
+login_url += "&username=" + username + "&password="+password+"&key=" + key
+login_req = opener.open(login_url)
+received_res = opener.open(get_url)
+received = received_res.read().decode("gbk")
+msgs = re.findall("<span class=\"msgContent\">(.*?)</span>",received,re.S)
+msgs.reverse()
+for msg in msgs:
+    wx.send_msg_to_user(msg,touser=["YifanLi"])
+print(msgs,)
